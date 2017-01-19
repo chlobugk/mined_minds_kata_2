b4e925874a9475a1bde3724946f6b746bc3f62dd
Chloe Krainbucher <chloekrainbucher@Chloes-MacBook-Pro.local>
2017-01-19 09:06:29 -0500

    first pass

--- test_functions.rb
+++ test_functions.rb
@@ -1,8 +1,16 @@
 require 'minitest/autorun'
 
+def mined_minds_function(number)
+	1
+end
 
 class TestMMFunctions < Minitest::Test
 	def test_one_equals_one()
 		assert_equal(1,1)
 	end
+
+	def test_pass_1_to_mined_minds_function_returns_1
+		assert_equal(1, mined_minds_function(1))
+	end
+
 end
